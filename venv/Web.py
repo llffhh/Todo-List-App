9cda0fd31a7fc1de47bcd577d02d667681ff48f5
llffhh <fhleong89@gmail.com>
2023-01-24 16:37:53 +0800

    implement the add function

--- venv/Web.py
+++ venv/Web.py
@@ -3,6 +3,13 @@ import module.function as function
 
 todos = function.readFile()
 
+def add_todo():
+    todo = st.session_state['new_todo'].title() + "\n"
+    todos.append(todo)
+    function.writeFile(todos)
+
+
+
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity")
@@ -10,4 +17,9 @@ st.write("This app is to increase your productivity")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="New todo: ", placeholder = "Add new todo...")
+st.text_input(label="New todo: ", placeholder = "Add new todo...",
+            on_change = add_todo, key = 'new_todo')
+
+st.session_state
+
+print("hello")
