73bf7f6da53ef04026913eb1691dbb6c5bdf28f4
llffhh <fhleong89@gmail.com>
2023-01-24 12:59:20 +0800

    implement complete and exit function

--- venv/gui.py
+++ venv/gui.py
@@ -8,9 +8,15 @@ add_button = sg.Button("Add")
 Listbox = sg.Listbox(values = function.readFile(), key = "todos", 
                     enable_events = True, size = [45,10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+
+exit_button = sg.Button("Exit")
+
 
 window = sg.Window('My To-Do App', 
-                    layout = [[label], [input_box, add_button], [Listbox, edit_button]],
+                    layout = [[label], [input_box, add_button], 
+                            [Listbox, edit_button, complete_button],
+                            [exit_button]],
                     font = ('Helvetica', 15))
 
 
@@ -32,6 +38,14 @@ while True:
             todos[index] = newTODO
             function.writeFile(todos)
             window["todos"].update(values = todos)
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos.remove(todo_to_complete)
+            function.writeFile(todos)
+            window['todos'].update(values = todos)
+            window['todo'].update(value = '')
+        case "Exit":
+            break
         case "todos":
             window['todo'].update(value = values['todos'][0])
         case sg.WIN_CLOSED:
